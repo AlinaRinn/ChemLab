3a449201ab99bafb3806db136171db6bab271552
AndreySyp <sammyva199@gmail.com>
2023-02-23 21:19:47 +0300

    Added the ability to take a bottle

--- scenes/main/gui.gd
+++ scenes/main/gui.gd
@@ -7,6 +7,7 @@ func set_target_name(name, _other):
 func _physics_process(_delta):
 	if !G.target_object:
 		set_target_name('', '')
-	elif G.target_object && 'target_name' in G.target_object:
-		set_target_name(G.target_object.target_name, 
-		G.target_object.target_other)
+	#не работает с подбором
+	#elif G.target_object && 'target_name' in G.target_object:
+		#set_target_name(G.target_object.target_name, 
+		#G.target_object.target_other)
