ced287e62c400eb7b27c922cae8f8b0bdf683571
AlinaRinn <nik17042001@rambler.ru>
2023-04-07 02:36:46 +0300

    New glass + gui & interfaces refactor

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -3,14 +3,14 @@ extends Control
 
 
 func _ready():
-	G.label_name = $target_name
-	G.label_bindings = $target_bindings
+	G.label_name = $ItemInfo/TargetName/label
+	G.label_bindings = $ItemInfo/TargetBindings/label
 	G.label_compounds = $target_compounds
 	G.label_compounds2 = $target_compounds2
 
 func set_target_name(name, _other):
-	$target_name.text = name
-	$target_bindings.text = ''
+	$ItemInfo/TargetName/label.text = name
+	$ItemInfo/TargetBindings/label.text = ''
 	$target_compounds.text = ''
 	$target_compounds2.text = ''
 
