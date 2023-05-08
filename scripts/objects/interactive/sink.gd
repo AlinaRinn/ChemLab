6409c8bf97558e3e80694ba160c780aa20bf3102
AlinaRinn <nik17042001@rambler.ru>
2023-05-08 20:59:03 +0300

    sink fix

--- scripts/objects/interactive/sink.gd
+++ scripts/objects/interactive/sink.gd
@@ -7,7 +7,9 @@ var water = {"name": "Вода", "color": Color.blue}
 
 func pour():
 	# Налить слой воды
-	if G.player.in_hand_object.has_method("add_substance"):
+	if G.player.in_hand_object == null:
+		pass
+	elif G.player.in_hand_object.has_method("add_substance"):
 		G.player.in_hand_object.add_substance(substance.water)
 	elif G.player.in_hand_object.has_method("add_liquid"):
 		G.player.in_hand_object.add_liquid(substance.water)
@@ -15,7 +17,9 @@ func pour():
 
 func drain():
 	# Удалить слой воды
-	if G.player.in_hand_object.has_method("remove_substance"):
+	if G.player.in_hand_object == null:
+		pass
+	elif G.player.in_hand_object.has_method("remove_substance"):
 		G.player.in_hand_object.remove_substance()
 	elif G.player.in_hand_object.has_method("remove_liquid"):
 		G.player.in_hand_object.remove_liquid()
