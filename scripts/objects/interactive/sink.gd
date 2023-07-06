e9611d0baa24405a783ff26b7d1f173117c8558a
AlinaRinn <nik17042001@rambler.ru>
2023-07-06 04:02:53 +0300

    some changes
    4 hours I tried to make a parallel playing for animations, but nothing worked

--- scripts/objects/interactive/sink.gd
+++ scripts/objects/interactive/sink.gd
@@ -11,9 +11,10 @@ func pour():
 		pass
 	elif G.player.in_hand_object.has_method("add_substance"):
 		G.player.in_hand_object.add_substance(substance.water)
+		$Pour.play("pour")
 	elif G.player.in_hand_object.has_method("add_liquid"):
 		G.player.in_hand_object.add_liquid(substance.water)
-
+		$Pour.play("pour")
 
 func drain():
 	# Удалить слой воды
@@ -21,5 +22,7 @@ func drain():
 		pass
 	elif G.player.in_hand_object.has_method("remove_substance"):
 		G.player.in_hand_object.remove_substance()
+		$Drain.play("drain")
 	elif G.player.in_hand_object.has_method("remove_liquid"):
 		G.player.in_hand_object.remove_liquid()
+		$Drain.play("drain")
