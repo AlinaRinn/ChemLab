3b952cacb26aedee07008d4b303ed2c1692d1985
Anton Kurashkevich <mr.kur.anton@gmail.com>
2024-06-24 18:20:46 +0300

    Fix 1.9 and add 1.10, 1.11, 2.2 labs

--- scripts/substance.gd
+++ scripts/substance.gd
@@ -10,9 +10,12 @@ export var sub = {
 	Phenolphthalein = {"name": "Фенолфталеин", "color": Color(0.960784, 0.960784, 0.862745, 0.1)},
 	LiCl = {"name": "Хлорид лития", "color": Color(0.960784, 0.960784, 0.960784, 0.5)},
 	Na2CO3 = {"name": "Карбонат натрия", "color": Color(0.662745, 0.662745, 0.662745, 0.5)},
+	Na2SO4 = {"name": "Сульфат натрия", "color": Color.white},
 	Ca = {"name": "Кальций+", "color": Color(0.752941, 0.752941, 0.752941, 0.5)}, # silver
 	Sr = {"name": "Стронций+", "color": Color(1, 1, 0, 0.5)}, # yellow
 	Ba = {"name": "Барий+", "color":  Color( 0.745098, 0.745098, 0.745098, 0.5)}, # gray
-	Mg = {"name": "Магний+", "color": Color(0.752941, 0.752941, 0.752941, 0.5) # silver
+	Mg = {"name": "Магний+", "color": Color(0.752941, 0.752941, 0.752941, 0.5)}, # silver
+	K2CrO4 = {"name": "Хромат калия", "color": Color.yellow },
+	Na2B4O7= {"name": "Тетраборат натрия", "color": Color.white 
 },
 }
