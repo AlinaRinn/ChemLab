62b77e7584aad7a88c2eb2f6215ec9f93b28ef4d
AlinaRinn <nik17042001@rambler.ru>
2024-12-02 00:16:20 +0300

    Grand rubicon settings refactor
    Settings load from file now

--- scripts/global.gd
+++ scripts/global.gd
@@ -45,7 +45,17 @@ var label_compounds2
 
 # settings
 var sensitivity = 0.005
-var screenState = true
+var Resolitions: Dictionary = {"3840x2160":Vector2(3840, 2160),
+								"2560x1440":Vector2(2560, 1440),
+								"1920x1080":Vector2(1920, 1080),
+								"1680x1050":Vector2(1680, 1050),
+								"1536x864":Vector2(1536, 864),
+								"1440x900":Vector2(1440, 900),
+								"1366x768":Vector2(1366, 768), 
+								"1280x720":Vector2(1280, 720),
+								"1024x768":Vector2(1024, 768),
+								"800x600":Vector2(800, 600)}
+
 
 func pause(p=true):
 	get_tree().paused = p
